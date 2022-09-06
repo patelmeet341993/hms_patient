4b7ca1da4668e38462e27a9f7a99607971e517bf
Dishant <agrawaldishant29@gmail.com>
2022-09-06 17:22:19 +0530

    modified HomeScreen to show Patients Length For Logged In Mobile Number and current patient in use

--- lib/views/homescreen/homescreen.dart
+++ lib/views/homescreen/homescreen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:patient/controllers/navigation_controller.dart';
+import 'package:patient/controllers/patient_controller.dart';
+import 'package:patient/models/patient_model.dart';
+import 'package:patient/providers/authentication_provider.dart';
+import 'package:patient/providers/patient_provider.dart';
+import 'package:patient/views/common/components/loading_widget.dart';
+import 'package:provider/provider.dart';
 
-import '../../controllers/firestore_controller.dart';
-import '../../utils/logger_service.dart';
+import '../../controllers/authentication_controller.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "/HomeScreen";
@@ -12,29 +18,69 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Future<void> futureGetPatientsData;
+
+  Future<void> getPatientsData() async {
+    AuthenticationProvider authenticationProvider = Provider.of(NavigationController.mainScreenNavigator.currentContext!, listen: false);
+
+    if(authenticationProvider.mobileNumber.isNotEmpty) {
+      List<PatientModel> patients = await PatientController().getPatientsForMobileNumber(mobileNumber: authenticationProvider.mobileNumber);
+      if(patients.isEmpty) {
+        PatientModel? patientModel = await PatientController().createPatient(mobile: authenticationProvider.mobileNumber);
+
+        await PatientController().getPatientsForMobileNumber(mobileNumber: authenticationProvider.mobileNumber);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    futureGetPatientsData = getPatientsData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
         appBar: AppBar(
-          title: Text("Home Screen"),
+          title: const Text("Home Screen"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                AuthenticationController().logout(context: context, isNavigateToLogin: true);
+              },
+              icon: const Icon(Icons.logout),
+            )
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("Login Body"),
-              FlatButton(
-                onPressed: () {
-                  FirestoreController().firestore.collection("temp").add({"dfgh" : "drdfgh"}).then((value) {
-                    Log().i("Document Created:${value.id}");
-                  });
+        body: FutureBuilder<void>(
+          future: futureGetPatientsData,
+          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+            if(snapshot.connectionState == ConnectionState.done) {
+              return Consumer<PatientProvider>(
+                builder: (BuildContext context, PatientProvider patientProvider, Widget? child) {
+                  PatientModel? currentPatient = patientProvider.getCurrentPatient();
+                  return Center(
+                    child: Column(
+                      // mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("${patientProvider.patientsLength} Patients Available"),
+                        Text(currentPatient != null ? "${currentPatient.id} Patient In Use" : "No Current Patient Available"),
+                      ],
+                    ),
+                  );
                 },
-                child: Text("Add"),
-              ),
-            ],
-          ),
+              );
+            }
+            else {
+              return const Center(
+                child: LoadingWidget(),
+              );
+            }
+          },
         ),
       ),
     );
