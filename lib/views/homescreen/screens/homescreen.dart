68c7d20121078e0c501983c4f912e1482e4bc5eb
DELL <virendesaivd77@gmail.com>
2022-09-07 11:34:41 +0530

    getting patients data in New Home Screen

--- lib/views/homescreen/screens/homescreen.dart
+++ lib/views/homescreen/screens/homescreen.dart
@@ -4,9 +4,13 @@ import 'package:patient/views/profile/screens/profilescreen.dart';
 import 'package:provider/provider.dart';
 
 import '../../../configs/styles.dart';
-import '../../../controllers/firestore_controller.dart';
+import '../../../controllers/authentication_controller.dart';
+import '../../../controllers/navigation_controller.dart';
+import '../../../controllers/patient_controller.dart';
+import '../../../models/patient_model.dart';
 import '../../../packages/flux/widgets/bottom_navigation_bar/bottom_navigation_bar.dart';
-import '../../../utils/logger_service.dart';
+import '../../../providers/authentication_provider.dart';
+import '../../common/components/loading_widget.dart';
 import '../../my_prescription/screens/prescription_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -20,6 +24,31 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late ThemeData themeData;
 
+  late Future<void> futureGetPatientsData;
+
+  Future<void> getPatientsData() async {
+    AuthenticationProvider authenticationProvider = Provider.of(NavigationController.mainScreenNavigator.currentContext!, listen: false);
+
+    if(authenticationProvider.mobileNumber.isNotEmpty) {
+      List<PatientModel> patients = await PatientController().getPatientsForMobileNumber(mobileNumber: authenticationProvider.mobileNumber);
+      if(patients.isEmpty) {
+        PatientModel? patientModel = await PatientController().createPatient(mobile: authenticationProvider.mobileNumber);
+
+        if(patientModel != null) {
+          await PatientController().getPatientsForMobileNumber(mobileNumber: authenticationProvider.mobileNumber);
+        }
+        else {
+          AuthenticationController().logout(context: context, isNavigateToLogin: true);
+        }
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    futureGetPatientsData = getPatientsData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,44 +57,60 @@ class _HomeScreenState extends State<HomeScreen> {
       color: themeData.backgroundColor,
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: FxBottomNavigationBar(
-          containerDecoration: BoxDecoration(
-            color: Styles.cardColor,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16), topRight: Radius.circular(16)),
-          ),
-          activeContainerColor:themeData.primaryColor.withAlpha(30),
-          fxBottomNavigationBarType: FxBottomNavigationBarType.containered,
-          showActiveLabel: false,
-          showLabel: false,
-          activeIconSize: 24,
-          iconSize: 24,
-          activeIconColor: themeData.primaryColor,
-          iconColor: Colors.grey,
-          itemList: [
-            FxBottomNavigationBarItem(
-              page: VisitScreen(),
-              activeIconData: Icons.house,
-              iconData: Icons.house_outlined,
-            ),
-            FxBottomNavigationBarItem(
-              page: MyPrescriptionScreen(),
-              activeIconData: Icons.date_range,
-              iconData: Icons.date_range_outlined,
-            ),
-            // FxBottomNavigationBarItem(
-            //   page: Container(),
-            //   activeIconData: Icons.chat_bubble,
-            //   iconData: Icons.chat_bubble_outline_rounded,
-            // ),
-            FxBottomNavigationBarItem(
-              page: ProfileScreen(),
-              activeIconData: Icons.person,
-              iconData: Icons.person_outline_rounded,
-            ),
-          ],
+        body: FutureBuilder<void>(
+          future: futureGetPatientsData,
+          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+            if(snapshot.connectionState == ConnectionState.done) {
+              return getMainBody();
+            }
+            else {
+              return const Center(
+                child: LoadingWidget(),
+              );
+            }
+          },
         ),
       ),
+    );
+  }
+
+  Widget getMainBody() {
+    return FxBottomNavigationBar(
+      containerDecoration: BoxDecoration(
+        color: Styles.cardColor,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16), topRight: Radius.circular(16)),
+      ),
+      activeContainerColor:themeData.primaryColor.withAlpha(30),
+      fxBottomNavigationBarType: FxBottomNavigationBarType.containered,
+      showActiveLabel: false,
+      showLabel: false,
+      activeIconSize: 24,
+      iconSize: 24,
+      activeIconColor: themeData.primaryColor,
+      iconColor: Colors.grey,
+      itemList: [
+        FxBottomNavigationBarItem(
+          page: VisitScreen(),
+          activeIconData: Icons.house,
+          iconData: Icons.house_outlined,
+        ),
+        FxBottomNavigationBarItem(
+          page: MyPrescriptionScreen(),
+          activeIconData: Icons.date_range,
+          iconData: Icons.date_range_outlined,
+        ),
+        // FxBottomNavigationBarItem(
+        //   page: Container(),
+        //   activeIconData: Icons.chat_bubble,
+        //   iconData: Icons.chat_bubble_outline_rounded,
+        // ),
+        FxBottomNavigationBarItem(
+          page: ProfileScreen(),
+          activeIconData: Icons.person,
+          iconData: Icons.person_outline_rounded,
+        ),
+      ],
     );
   }
 }
