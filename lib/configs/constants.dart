cb8af5d025e7c72b3ad568838ccbdb3aa73b0306
hpy <happypatel379@gmail.com>
2022-12-08 17:48:40 +0530

    remove unnecessary constants

--- lib/configs/constants.dart
+++ lib/configs/constants.dart
@@ -6,10 +6,3 @@ class SharePrefrenceKeys {
   static const String appThemeMode = "themeMode";
   static const String loggedInUser = "loggedInUser";
 }
-
-class TreatmentActivityStatus {
-  static const String billPay = "billPay";
-  static const String registered = "registered";
-  static const String prescribed = "prescribed";
-  static const String completed = "completed";
-}
